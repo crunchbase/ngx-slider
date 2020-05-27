7c16df40d1cbe76485170ba61b8e00d3e990f797
Mark Emmanuel Diez <mdiez@crunchbase.com>
2020-05-27 13:36:35 -0700

    Fix upload creds

--- build/bin/upload.sh
+++ build/bin/upload.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
-set -e
+set -xe
 
-AUTH=$(echo "$ARTIFACTORY_USERNAME:$ARTIFACTORY_PASSWORD" | openssl base64)
-
-SCOPE=crunchbase
+SCOPE="@crunchbase"
+EMAIL="ci@crunchbase.com"
 REGISTRY=http://artifactory5.crunchbase.io/artifactory/api/npm/npm-local/
 
-cat >> ~/.npmrc <<EOF
+AUTH=$(echo -n "$ARTIFACTORY_USERNAME:$ARTIFACTORY_PASSWORD" | openssl base64)
+BASE64_PASSWORD=$(echo -n "$ARTIFACTORY_PASSWORD" | openssl base64)
+
+# Yes, it needs both "global" auth and "scoped" auth
+cat > ~/.npmrc <<EOF
 _auth = ${AUTH}
-email = ci@crunchbase.com
+email = ${EMAIL}
 always-auth = true
+
+${SCOPE}:registry=http://artifactory5.crunchbase.io/artifactory/api/npm/npm-local/
+//artifactory5.crunchbase.io/artifactory/api/npm/npm-local/:_password=${BASE64_PASSWORD}
+//artifactory5.crunchbase.io/artifactory/api/npm/npm-local/:username=${ARTIFACTORY_USERNAME}
+//artifactory5.crunchbase.io/artifactory/api/npm/npm-local/:email=${EMAIL}
+//artifactory5.crunchbase.io/artifactory/api/npm/npm-local/:always-auth=true
 EOF
 
 npm publish --registry http://artifactory5.crunchbase.io/artifactory/api/npm/npm-local/
+
